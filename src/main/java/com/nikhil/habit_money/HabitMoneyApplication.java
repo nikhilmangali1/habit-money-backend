bc5900ba352526a991f5dd6f83eae6f77f9da92f
nikhilmangali1 <nikhilmangali158@gmail.com>
2026-07-18 02:26:40 +0530

    first commit: habit-money-backend

---
+++ src/main/java/com/nikhil/habit_money/HabitMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.habit_money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitMoneyApplication.class, args);
+	}
+
+}
